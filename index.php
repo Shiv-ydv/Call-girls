2cb2a36750f6c94680743f090b16176f7bfa186a
Shiv-ydv <shivyadav5648@gmail.com>
2024-05-31 15:51:52 +0530

    ok

---
+++ index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Call Girls Mumbai</title>
+  </head>
+  <body>
+    <section class="topbg">
+      <div class="container"><br>
+        <div class="col-md-12">
+          <h2 class="top_header text-center">HIGH PROFILE MODELS @ ₹5500 TO 25K WITH AC ROOM</h2>
+          <center><img src="img/ban.png" alt="" class="img-fluid"></center>
+        </div>
+      </div>
+    </section>
+    
+    <section class="bannerbg">
+      <div class="container">
+       
+          <div class="col-md-5">
+          <div class="sitename"><h2>Manisha Malik</h2></div>
+          <div class="banner_heading">150+ Charming Call Girls Ready For You</div>
+          <div class="banner_text text-justify">Hi guys. My name is Manisha, a seductive 24-year-old blonde willing to spend some erotic time with you. I believe that you are already attracted to me because of my look but stay assured that I have much to show you. I will provide you with 100% sexual satisfaction. Call me.</div>
+         
+          <div class="row">
+        <div class="col-md-6 col-sm-12 mt-3"><div class="banner_button"><a href="https://www.madhuescorts.com/whatsapp-9905881744.html">Call Now</a></div></div>
+          <div class="col-md-6 col-sm-12 mt-3"><div class="banner_button2"><a href="https://www.madhuescorts.com/whatsapp-9905881744.html">Whatsapp Now</a> </div></div>
+          </div>
+          <div class="col-md-7 modelimg">
+          
+          <!-- <img src="img/manish.jpg" width="500" height="500" alt="Delhi call girls whatsapp Number" title="Manisha Malik Delhi escorts Hot Service"> -->
+          
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section STYLE="background-color:red;">
+      <div class="container"><br>
+        <h2 class="top_header text-center">MUMBAI ESCORTS ARE PERFECT COMPANIONS</h2>
+        <p class="banner_text text-justify">Who can be your perfect partner? Do you have a definition? Well, not always do you have a definition of what your perfect partner looks like. But you know your sexual wants. You know about your needs and what feeling you would like to receive. Connect with the <a href=""class="text-warning"><b>Delhi Escorts</b></a> to get the best partner by your side. <br><br>
+         Our firm holds the best list of beguiling <a href="" class="text-warning"><b>VIP call girls Delhi</b></a>. These classy babes have always stood up to be the efficacious professionals in giving complete time to the customers. Your sexual needs will be recognized and fulfilled by these girls. Exciting your sexual wants, they can make the best approach to give you a complete time of lovemaking. <br><br>
+
+         Definitely you will find a lot of professionals in this industry. But do they possess the features that make them superior to the rest? Well, not all escorts are capable of giving you a high-fi sexual time. They do not even match your sexual desires. Some only provide normal sexual treats to their customers. We believe that you never with something like that. You want something special. You wish each of your nerves to feel excitement, thrill, and lastly ultimate pleasure through their services. For the same, we do have an excellent category of independent escorts Delhi.</p>
+      </div>
+
+      <div class="container">
+      <div class="row mt-4 my-3">
+        <div class="col-md-6 ">
+          <img src="img/manisha-banner-2.webp" alt="" class="img-fluid w-100">
+        </div>
+        <div class="col-md-6">
+        <h2 class="top_header text-center">HOW DO OUR INDEPENDENT DELHI ESCORTS STAND EXCLUSIVE FROM THE OTHERS?</h2>
+        <p class="banner_text">Our independent Delhi escorts possess the elegance to stand by the side of the customers. And the skill to provide complete sexual time.</p>
+       <p class="banner_text mt-3">The features that make our escorts different from the rest sexual service providers in this industry are;</p>
+      </div>
+      </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12"><p class="banner_text">We urge you to check on the category of our ravishing escorts in Delhi. You will appreciate the looks of these girls. They hold the factors in them that make them different from the rest.</p></div><br>
+    <div class="col-md-12 mb-5"><p class="banner_text">In one word, our escorts are beautiful. They possess charismatic eyes that always pull customers towards them. Their pink lips are plumpy and juicy. And they are good-looking. They are beautiful enough to catch the eyeballs of customers. Thus, it will be impossible for you to turn your eyeballs from these <a href="" class="text-warning"><b>independent call girls Delhi</b></a>.</p></div>
+  </div>
+</div>
+    </section>
+
+    <!-- section33 parts -->
+    <section style="background-color:black;">
+      <div class="container"><br><br>
+        <h2 class="top_header text-center text-warning">OUR ALL ESCORTS GIRLS</h2>
+        <h2 class="top_header text-center">BOOK EXTREME NAUGHTY ESCORT MODEL</h2>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <img src="img/manisha-malik-model-2.webp" alt="" class="img-fluid mt-2">
+            <div class="details">
+              <h6 class="text-center">MIRA MEHTA</h6>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <img src="img/manisha-malik-model-3.webp" alt="" class="img-fluid mt-2">
+            <div class="details">
+              <h6 class="text-center">NISHA RATHI</h6>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <img src="img/manisha-malik-model-5.webp" alt="" class="img-fluid mt-2">
+            <div class="details">
+              <h6 class="text-center">RHEA JHOSI</h6>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <img src="img/manisha-malik-model-10.webp" alt="" class="img-fluid mt-2">
+            <div class="details">
+              <h6 class="text-center">SARA TRIVEDI</h6>
+            </div>
+            </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <img src="img/manisha-malik-model-12.webp" alt="" class="img-fluid mt-2">
+            <div class="details">
+              <h6 class="text-center">MIRA MEHTA</h6>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <img src="img/manisha-malik-model-13.webp" alt="" class="img-fluid mt-2">
+            <div class="details">
+              <h6 class="text-center">NISHA RATHI</h6>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <img src="img/manisha-malik-model-16.webp" alt="" class="img-fluid mt-2">
+            <div class="details">
+              <h6 class="text-center">RHEA JHOSI</h6>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <img src="img/manisha-malik-model-19.webp" alt="" class="img-fluid mt-2">
+            <div class="details">
+              <h6 class="text-center">SARA TRIVEDI</h6>
+            </div>
+            </div>
+        </div>
+      </div>
+    </section>
+
+    <section style="background-color:red;">
+      <div class="container"><br><br>
+        <h2 class="text-light text-center">MUMBAI ESCORTS SERVICE COMES WITH A BONA FIDE TOUCH</h2>
+        <p class="banner_text text-justify">Many things together make our <a href="" class="text-warning"><b>Mumbai Escorts Service</b></a> the best of the rest. Here we can’t just categorize the best things and worst things in our services. But we can tell you that our services contain the most mesmerizing features that will surprise you and will give you the actual taste of satiation. Well, we are careful in adding features to our services. We make sure that every customer receives the best from us. <br><br>
+
+We do understand what makes you happy. And have filled our services with the same. Certainly, a single service doesn’t hold everything that customers crave. We do provide a versatile range of services that are uniquely designed to fulfill the wants of men. We definitely have something for you. Just go through our list of services and you will be able to understand why we are recommended by customers. <br><br>
+
+You will find a huge number of services that are exotically designed for you. Thus, every service holds some uniqueness. So, make sure that you know about your sexual needs, and then check our list of services to pick the best-suited one for you.
+
+Till date, none of our customers have been disappointed with our services. And we are pretty sure that you will have a great time to spend with our <a href="" class="text-warning"><b>Call Girls In Mumbai</b></a>. If you do not find anything matching your needs, we request you to talk with our executives who will help you in finding something that fits your wants. <br><br>
+
+Do we provide a redesigned sexual service? Yes, we do… A number of times, men have approached us desiring something more or less from what we provide. Some want to add some moves. Some wish to include role roleplays in their classic services. Well, that is okay for us. We always work according to the desire of the customers. And that is why we make the changes that make it fulfilling. <br><br>
+
+If you think that is all in our services then you are wrong. The best part of our firm is that we provide bona fide services. No matter what service you are going to choose you will find the same in the best form and bearing all its reality. <br><br>
+
+You invest in taking erotic service. And will never want to get cheated by your service provider. Well, if you listen to the stories of this industry, then you will know about many firms that provide unreal sexual services. They do cheat their customers in the name of actual sexual services. One expects to get a fantastic sexual time whereas they receive a quickie. There is no emotion in such services. And no attraction or foreplay.</p> <br>
+<div class="row">
+  <div class="col-md-6"><img src="img/manisha-banner-3.webp" alt="" class="img-fluid"></div>
+  <div class="col-md-6"><img src="img/manisha-banner-4.webp" alt="" class="img-fluid"></div>
+</div>      
+</div>
+    </section>
+
+
+    <section style="background-color:black;">
+      <div class="container"><br><br>
+        <h1 class="text-warning top_header text-center">MOST POPULAR ESCORTS GIRLS</h1>
+        <h5 class="top_header text-center" >OUR NEW HOT SERVICES OFFER BY MANISHA MALIK</h5>
+        <div class="row">
+          <div class="col-md-3"><img src="img/manisha-malik-model-30.webp" alt="" class="img-fluid">
+          <div class="detail">
+              <h6 class="text-center"><B>AVAILABLE NOW</B></h6>
+            </div>
+        </div>
+          <div class="col-md-3"><img src="img/manisha-malik-model-31.webp" alt="" class="img-fluid">
+          <div class="detail">
+              <h6 class="text-center"><B>AVAILABLE NOW</B></h6>
+            </div>
+        </div>
+          <div class="col-md-3"><img src="img/manisha-malik-model-32.webp" alt="" class="img-fluid">
+          <div class="detail">
+              <h6 class="text-center"><B>AVAILABLE NOW</B></h6>
+            </div>
+        </div>
+          <div class="col-md-3"><img src="img/manisha-malik-model-33.webp" alt="" class="img-fluid">
+          <div class="detail">
+              <h6 class="text-center"><B>AVAILABLE NOW</B></h6>
+            </div>
+        </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3"><img src="img/manisha-malik-model-34.webp" alt="" class="img-fluid">
+          <div class="detail">
+              <h6 class="text-center"><B>AVAILABLE NOW</B></h6>
+            </div>
+        </div>
+          <div class="col-md-3"><img src="img/manisha-malik-model-35.webp" alt="" class="img-fluid">
+          <div class="detail">
+              <h6 class="text-center"><B>AVAILABLE NOW</B></h6>
+            </div>
+        </div>
+          <div class="col-md-3"><img src="img/manisha-malik-model-36.webp" alt="" class="img-fluid">
+          <div class="detail">
+              <h6 class="text-center"><B>AVAILABLE NOW</B></h6>
+            </div>
+        </div>
+          <div class="col-md-3"><img src="img/manisha-malik-model-37.webp" alt="" class="img-fluid">
+          <div class="detail">
+              <h6 class="text-center"><B>AVAILABLE NOW</B></h6>
+            </div>
+        </div>
+        </div>
+      </div>
+    </section>
+<?php include "footerlink.php"?>
+</body>
+</html>
